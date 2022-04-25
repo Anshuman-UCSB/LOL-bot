5bd322cfa2cf3d7ab3e9b6aa07f47ffb19bec740
Anshuman Dash <anshuman@ucsb.edu>
2022-04-24 19:32:28 -0700

    updated by script

--- lcudriver.py
+++ lcudriver.py
@@ -12,18 +12,18 @@ async def connect(connection):
 	queueUp = await connection.request('post','/lol-lobby/v2/lobby/matchmaking/search')
 	print(await queueUp.json())
 
-	queueResponse = "initialized"
-	while(queueResponse != "InProgress"):
-		queuePop = await connection.request('get','/lol-matchmaking/v1/ready-check')
-		resp = await queuePop.json()
-		print(dict(resp))
-		queueResponse = (dict(resp)['state'])
-		print(queueResponse)
-		sleep(2)
+	# queueResponse = "initialized"
+	# while(queueResponse != "InProgress"):
+	# 	queuePop = await connection.request('get','/lol-matchmaking/v1/ready-check')
+	# 	resp = await queuePop.json()
+	# 	print(dict(resp))
+	# 	queueResponse = (dict(resp)['state'])
+	# 	print(queueResponse)
+	# 	sleep(2)
 	
 
-	accept = await connection.request('post','/lol-matchmaking/v1/ready-check/decline')
-	print(await accept.json())
+	# accept = await connection.request('post','/lol-matchmaking/v1/ready-check/decline')
+	# print(await accept.json())
 
 if __name__ == "__main__":
 	connector.start()
