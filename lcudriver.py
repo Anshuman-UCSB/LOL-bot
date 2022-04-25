af1708aeffef527111d651dd2f97f58b73d0922f
Anshuman Dash <anshuman@ucsb.edu>
2022-04-24 20:04:24 -0700

    updated by script

--- lcudriver.py
+++ lcudriver.py
@@ -8,10 +8,48 @@ async def getIdName(connection):
 	json = await req.json()
 	return (json['accountId'],json['displayName'])
 
+async def invite(connection, id, name):
+	myid, myName = await getIdName(connection)
+	body = {
+			"eligibility": {
+				"eligible": true,
+				"queueId": 0,
+				"restrictions": [
+				{
+					"expiredTimestamp": 0,
+					"restrictionArgs": {
+					"additionalProp1": "string",
+					"additionalProp2": "string",
+					"additionalProp3": "string"
+					},
+					"restrictionCode": "QueueDisabled",
+					"summonerIds": [
+					0
+					],
+					"summonerIdsString": "string"
+				}
+				]
+			},
+			"errorType": "string",
+			"fromSummonerId": myId,
+			"fromSummonerName": myName,
+			"id": "string",
+			"invitationMetaData": {
+				"additionalProp1": {}
+			},
+			"state": "Requested",
+			"timestamp": "string",
+			"toSummonerId": id,
+			"toSummonerName": name,
+		}
+	req = await connection.request('post', '/lol-lobby/v1/lobby/invitations')
+	pass
+
 @connector.ready
 async def connect(connection):
 
 	print(await getIdName(connection))
+	await invite(connection,*(243907053, 'Hevraz'))
 	# await connection.request('post', '/lol-lobby/v2/lobby', data={"queueId":430})
 	
 	# await connection.request('post','/lol-lobby/v2/lobby/matchmaking/search')
