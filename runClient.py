09eaa1f9e7dcc20cbb7e4e1fbfdfdd4c44109df8
Anshuman Dash <anshuman@ucsb.edu>
2022-05-04 10:44:12 -0700

    :)

--- runClient.py
+++ runClient.py
@@ -2,7 +2,7 @@ from subprocess import run
 import traceback
 run(['git','pull'])
 try:
-	from client import client
+	import client.client as client
 	client.main()
 except Exception as ex:
     traceback.print_exc()
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('dotenv').config()
-
 const app = express();
+
+var config = {
+    
+}
 
 app.get("/register", (req, res) => {
 
@@ -11,6 +14,10 @@ app.get("/instr/:id", (req, res) => {
 
 })
 
+app.get("lobby", (req, res) => {
+    res.json()
+})
+
 app.post("/tasks/:id", (req, res) => {
 
 })
@@ -18,6 +25,7 @@ app.post("/tasks/:id", (req, res) => {
 app.post("/lobby", (req, res) => {
 
 })
+
 
 
 app.listen(process.env.PORT, () => {
