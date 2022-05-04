fbc90cf3eab789256d70ecd96207d866151ba112
Anshuman Dash <anshuman@ucsb.edu>
2022-05-04 10:43:08 -0700

    :)

--- runClient.py
+++ runClient.py
@@ -2,7 +2,7 @@ from subprocess import run
 import traceback
 run(['git','pull'])
 try:
-	import client
+	from client import client
 	client.main()
 except Exception as ex:
     traceback.print_exc()
