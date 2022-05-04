f2db5d5b5a303e89dac3bcf707d3be7be1455d99
Anshuman Dash <anshuman@ucsb.edu>
2022-05-04 10:44:46 -0700

    :)

--- runClient.py
+++ runClient.py
@@ -1,8 +1,10 @@
 from subprocess import run
+import sys
+sys.path.insert(0,'./client')
 import traceback
 run(['git','pull'])
 try:
-	import client.client as client
+	import client
 	client.main()
 except Exception as ex:
     traceback.print_exc()
